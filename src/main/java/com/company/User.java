8f59cf7952dbf34449d2b1de8047af7f7b66c9ed
Anton Sidlyar <antonsidlyar@gmail.com>
2021-05-27 00:56:45 +0300

    Возобновил проект у себя на ПК

---
+++ src/main/java/com/company/User.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public final class User {
+
+    public static UserData userData;
+    public static RSAEncryption rsa;
+
+
+}
